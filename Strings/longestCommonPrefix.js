aa38df90830aed3343576e1edc49c7f6ea2dd2ce
raram2 <gkfka1189@gmail.com>
2020-10-08 21:08:51 +0900

    Update longestCommonPrefix.js

--- Strings/longestCommonPrefix.js
+++ Strings/longestCommonPrefix.js
@@ -1,11 +1,24 @@
 /* Helper */
+var err = new Error('value is not string');
+
+/**
+ * @param {any} value
+ * @return {boolean}
+ */
 var isString = (value) => typeof value === 'string';
 
+/**
+ * @param {string} main
+ * @param {string[]} rests
+ * @param {number} i
+ * @param {string} prefix
+ * @return {string}
+ */
 var makePrefix = function (main, rests, i, prefix) {
   if (prefix.length === main.length) return prefix;
   const isCommon = rests.every((str) => {
     if (!isString(str[i]) || !isString(main[i])) {
-      throw new TypeError('value must be string');
+      throw err; // break the process
     }
     return str[i] === main[i];
   });
@@ -13,9 +26,11 @@ var makePrefix = function (main, rests, i, prefix) {
 };
 
 /* Main */
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
 var longestCommonPrefix = function (strs) {
   if (!strs.length) return '';
   return makePrefix(strs.shift(), strs, 0, '');
 };
-
-/* Pseudo */
